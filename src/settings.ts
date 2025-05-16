7d45f508eb6362205a7258f4cbff48a11de6bd67
laba0002 <laba0002@stud.kea.dk>
2025-05-16 11:13:53 +0200

    init

---
+++ src/settings.ts
@@ -0,0 +1,4 @@
+export const apiDetails = {
+  supabaseUrl: "https://bpwqftvfdrsmvgwzbzpm.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwd3FmdHZmZHJzbXZnd3pienBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDg3NTksImV4cCI6MjA2MjcyNDc1OX0.5qhkUpWBopIiQODX6aLqRkR-ZfkMYLizLXOqwb_x0wk",
+};
