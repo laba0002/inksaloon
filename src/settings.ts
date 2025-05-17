0ddefe2a1414096d87ca586d23657e9ab703036d
laba0002 <laba0002@stud.kea.dk>
2025-05-17 12:42:55 +0200

    init produktliste

---
+++ src/settings.ts
@@ -0,0 +1,4 @@
+export const apiDetails = {
+  supabaseUrl: "https://ztoxevkkigirigxlixqv.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0b3hldmtraWdpcmlneGxpeHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NzI0MDksImV4cCI6MjA2MzA0ODQwOX0.d8_p3IR8hU1HeawdScepcGij-eoPUB32SS_F0ri2I5M",
+};
