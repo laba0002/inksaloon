314463f8fab00a10beac1b5db68dd377ac68fe51
laba0002 <laba0002@stud.kea.dk>
2025-05-19 13:46:35 +0200

    nye apikeys + billeder på

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export const apiDetails = {
-  supabaseUrl: "https://ztoxevkkigirigxlixqv.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0b3hldmtraWdpcmlneGxpeHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0NzI0MDksImV4cCI6MjA2MzA0ODQwOX0.d8_p3IR8hU1HeawdScepcGij-eoPUB32SS_F0ri2I5M",
+  supabaseUrl: "https://bpwqftvfdrsmvgwzbzpm.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwd3FmdHZmZHJzbXZnd3pienBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDg3NTksImV4cCI6MjA2MjcyNDc1OX0.5qhkUpWBopIiQODX6aLqRkR-ZfkMYLizLXOqwb_x0wk",
 };
